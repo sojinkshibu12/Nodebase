c9fea77ec948c9d9d0f63e1c78a9579d4c9be583
sojinkshibu12 <sojinkshibu@gmail.com>
2026-02-08 17:52:40 +0530

    added updating name function header

--- src/app/functionalities/workflows/servers/prefetch.ts
+++ src/app/functionalities/workflows/servers/prefetch.ts
@@ -6,3 +6,7 @@ type Input = inferInput<typeof trpc.workflows.getmany>;
 export const prefetchworkflows = (params:Input)=>{
     return prefetch(trpc.workflows.getmany.queryOptions(params));
 }
+
+export const prefetchoneworkflows = (id:string)=>{
+    return prefetch(trpc.workflows.getone.queryOptions({id}));
+}
